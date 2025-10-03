2649862c9320d2aa20f3058b99879c3c11e1924e
madradretrotoys <madradretrotoys@gmail.com>
2025-10-02 18:57:49 -0600

    Create logout.ts

---
+++ functions/api/auth/logout.ts
@@ -0,0 +1,15 @@
+export const onRequestPost: PagesFunction = async () => {
+  const cookie = [
+    "rp_jwt=",
+    "Path=/",
+    "HttpOnly",
+    "Secure",
+    "SameSite=Lax",
+    "Max-Age=0",
+    "Expires=Thu, 01 Jan 1970 00:00:00 GMT"
+  ].join("; ");
+  return new Response(JSON.stringify({ ok: true }), {
+    status: 200,
+    headers: { "content-type": "application/json", "set-cookie": cookie }
+  });
+};
