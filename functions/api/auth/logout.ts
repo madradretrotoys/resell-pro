dcb75aba6b2d6ceaca2cdbf5565cd9da6467015c
madradretrotoys <madradretrotoys@gmail.com>
2025-10-03 09:55:37 -0600

    Update logout.ts

--- functions/api/auth/logout.ts
+++ functions/api/auth/logout.ts
@@ -1,13 +1,14 @@
 export const onRequestPost: PagesFunction = async () => {
-    const cookie = [
-    "rp_jwt=",
-    "Path=/",
-    "HttpOnly",
-    "Secure",
-    "SameSite=Lax",
-    "Max-Age=0",
-    "Expires=Thu, 01 Jan 1970 00:00:00 GMT"
-  ].join("; ");
+  const cookie = [
+      "rp_jwt=",
+      "Path=/",
+      "Domain=.resell-pro.pages.dev",
+      "HttpOnly",
+      "Secure",
+      "SameSite=Lax",
+      "Max-Age=0",
+      "Expires=Thu, 01 Jan 1970 00:00:00 GMT"
+    ].join("; ");  
   return new Response(JSON.stringify({ ok: true }), {
     status: 200,
     headers: { "content-type": "application/json", "set-cookie": cookie }
