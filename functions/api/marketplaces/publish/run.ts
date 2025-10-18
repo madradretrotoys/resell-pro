10c71c90044ad4abdd4b0fec8b1861c247e3d339
madradretrotoys <madradretrotoys@gmail.com>
2025-10-18 14:07:07 -0600

    Update run.ts

--- functions/api/marketplaces/publish/run.ts
+++ functions/api/marketplaces/publish/run.ts
@@ -1,4 +1,4 @@
-function json(status: number, data: unknown) {
+function json(data: unknown, status = 200) {
   return new Response(JSON.stringify(data), {
     status,
     headers: { "content-type": "application/json; charset=utf-8" },
